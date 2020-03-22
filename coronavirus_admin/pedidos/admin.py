08a4ac8262932923f713024cc1cf2cf20639d6f7
Edimar Manica <edimarmanica@gmail.com>
2020-03-22 20:11:03 -0300

    adicionado a restrição de número de produtos ativos por empresa

--- coronavirus_admin/pedidos/admin.py
+++ coronavirus_admin/pedidos/admin.py
@@ -1,5 +1,5 @@
 #Classes do django
-from django.contrib import admin
+from django.contrib import admin, messages
 
 #Classe do projeto
 from pedidos.models import Pedido, Produto, CategoriaProduto, PedidoProduto, Endereco, StatusPedido 
@@ -17,6 +17,24 @@ class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('descricao', 'empresa', 'ativo', 'valor')
     list_filter = ('empresa', 'ativo')   
     search_fields = ['descricao', ]
+    
+    def save_model(self, request, obj, form, change):
+        #se o produto está ativo, tem que verificar se não ultrapassa o número máximo de produtos ativos por empresa
+        if obj.ativo:
+            nr_produtos_ativos = Produto.objects.filter(empresa=obj.empresa, ativo=True).count()
+            
+            if change:#se é update só precisa verificar se o produto era inativo antes 
+                if not(Produto.objects.get(pk=obj.pk).ativo): #se o produto era inativo
+                    if obj.empresa.limite_produto <= nr_produtos_ativos:
+                        messages.set_level(request, messages.ERROR)
+                        messages.error(request, 'O limite de produtos ativos para a sua empresa foi atingido. O produto não foi alterado!')
+                        return
+            else: #se é insert, tem que verificar
+                if obj.empresa.limite_produto <= nr_produtos_ativos:
+                    messages.set_level(request, messages.ERROR)
+                    messages.error(request, 'O limite de produtos ativos para a sua empresa foi atingido. O produto não foi inserido!')
+                    return
+        super(ProdutoAdmin, self).save_model(request, obj, form, change)
     
     
 class EnderecoTabularInline(admin.TabularInline):
